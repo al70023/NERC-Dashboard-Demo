2cbe3ad8e72dda0204a65484c3fef6273407cec4
Alex Pantazopol <al70023@gmail.com>
2024-01-11 10:21:06 -0600

    Added react bootstrap table toolkit npm package

---
+++ react-bootstrap-table2-toolkit/lib/src/op/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _csv = require('react-bootstrap-table2-toolkit/lib/src/op/csv');
+
+var _csv2 = _interopRequireDefault(_csv);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = {
+  csvOperation: _csv2.default
+};
